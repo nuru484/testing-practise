9e00d1f9985013d357f338145bcadbf2d19baa13
Nurudeen Abdul-Majeede <abdulmajeednurudeen48@gmail.com>
2024-02-08 23:53:44 -0800

    Added the test case for reverseString function

--- src/index.test.js
+++ src/index.test.js
@@ -3,3 +3,7 @@ const indexModule = require('./index');
 test('Capitalize first character', () => {
   expect(indexModule.capitalize('abc')).toBe('Abc');
 });
+
+test('Reverse string', () => {
+  expect(indexModule.reverseString('abc')).toBe('cba');
+});
