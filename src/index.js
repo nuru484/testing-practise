10047c5102749236499781f32475733d3e0e58b7
Nurudeen Abdul-Majeede <abdulmajeednurudeen48@gmail.com>
2024-02-09 00:17:02 -0800

    Added calculator function to be tested

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ const reverseString = (str) => {
   return str.split('').reverse().join('');
 };
 
+const calculator = {
+  add: (a, b) => {},
+  subtract: (a, b) => {},
+  multiply: (a, b) => {},
+  divide: (a, b) => {},
+};
+
 module.exports = {
   capitalize,
   reverseString,
+  calculator,
 };
