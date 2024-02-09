7c48b037c430c06e6b75aad5a81b49a9558baa9a
Nurudeen Abdul-Majeede <abdulmajeednurudeen48@gmail.com>
2024-02-09 00:04:37 -0800

    Added code to the reverseString function to make it pass the test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ const capitalize = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
-const reverseString = (str) => {};
+const reverseString = (str) => {
+  return str.split('').reverse().join('');
+};
 
 module.exports = {
   capitalize,
