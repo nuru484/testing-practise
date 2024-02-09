b3af20d1fafefbf98d716ccbde6af6570be35532
Nurudeen Abdul-Majeede <abdulmajeednurudeen48@gmail.com>
2024-02-08 23:47:16 -0800

    Added reverseString function
    To fail test before implementation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const capitalize = (str) => {
   return str.charAt(0).toUpperCase() + str.slice(1);
 };
 
+const reverseString = (str) => {};
+
 module.exports = {
   capitalize,
+  reverseString,
 };
